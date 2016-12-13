3e39840cbd19df1716da6cc73fb629bd7582a60d
azamzamy <a.zamzamy@hotmail.com>
2016-12-13 03:12:07 +0200

    Working Soldier With Awesome Camera View. Jump animation not implemented. Scripts are javaScript not C#.

---
+++ Assets/Scripts/SoldierActions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierActions : MonoBehaviour {
+
+	private NavMeshAgent enemy;
+	public Transform target;
+	public Transform target2;
+	// Use this for initialization
+	void Start () {
+		enemy = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		enemy.SetDestination (target.position);
+
+		if (enemy.transform.position.x == target.position.x &&
+			enemy.transform.position.z == target.position.z) {
+			target = target2;
+		}
+	}
+}
